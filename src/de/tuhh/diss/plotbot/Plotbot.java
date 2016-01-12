dd94f877e9bddd214f1f5f57564b24f5fb9c74e6
auchingnok <auchingnok@gmail.com>
2016-01-12 14:29:14 +0100

    Andy

---
+++ src/de/tuhh/diss/plotbot/Plotbot.java
@@ -0,0 +1,14 @@
+package de.tuhh.diss.plotbot;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+
+public class Plotbot {
+	public static void main(String[] args)
+	{
+		// Some example code to check if the build process works
+		LCD.drawString("Compiled successfully", 0, 0);
+		LCD.drawString("Good Luck!", 0, 1);
+		Button.ESCAPE.waitForPressAndRelease();
+	}
+}
